16fce5d837bb28432c4bce22377cafedcf85fb36
iMushi <imushi@me.com>
2017-12-09 11:52:58 -0600

    Primer Conexión a mongoDB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose')
 var app = require('./app');
 var port = process.env.PORT || 5000;
+var mongoURI = process.env.MDBURI || "mongodb://heroku_tqf52q58:q3uoqqehp1547bb721tt7e1872@ds133746.mlab.com:33746/heroku_tqf52q58";
 
 mongoose.Promise = global.Promise;
 
@@ -18,6 +19,16 @@ mongoose.connect('mongodb://localhost:27017/ZOO',{useMongoClient:true}).then(
 */
 
 
-app.listen(port,_ => {
-    console.log('Servidor Corriendo');
-});
+mongoose.connect(mongoURI,{useMongoClient:true}).then(
+    _ => {
+        console.log("conectado a MongoDB");
+
+        app.listen(port,_ => {
+            console.log('Servidor Corriendo');
+        });
+
+    }
+).catch(
+    err => console.log("Error al conectar a MongoDB =====> " + err)
+)
+
