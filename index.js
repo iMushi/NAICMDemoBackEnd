d1db45f7e42a4bacf59ef21674dc5037507dd0f4
iMushi <imushi@me.com>
2018-01-18 12:11:57 -0600

    cambios para digitalOcean y para reseteo de información

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ var https = require('https');
 var port = process.env.PORT || 5000;
 var mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/ZOO"; //Configuración del servidor o localhost
 
-//mongoURI = "mongodb://heroku_tqf52q58:q3uoqqehp1547bb721tt7e1872@ds133746.mlab.com:33746/heroku_tqf52q58";
-mongoose.Promise = global.Promise;
+mongoURI = "mongodb://heroku_tqf52q58:q3uoqqehp1547bb721tt7e1872@ds133746.mlab.com:33746/heroku_tqf52q58";
 
+
+
+mongoose.Promise = global.Promise;
 const Mongod = require('mongod');
 
 // Simply pass the port that you want a MongoDB server to listen on.
@@ -20,10 +22,10 @@ const Mongod = require('mongod');
 //});
 
 
-//var privateKey  = fs.readFileSync('certificates/key.pem', 'utf8');
-//var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
+var privateKey  = fs.readFileSync('certificates/key.pem', 'utf8');
+var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
 
-//var credentials = {key: privateKey, cert: certificate};
+var credentials = {key: privateKey, cert: certificate};
 
 //server.open((err) => {
 //    console.log(err);
@@ -34,14 +36,14 @@ const Mongod = require('mongod');
                 console.log("conectado a MongoDB ====> " + mongoURI);
 
 
-                app.listen(port, _ => {
+//                app.listen(port, _ => {
 
-                    console.log('Servidor Corriendo');
-                });
+  //                  console.log('Servidor Corriendo');
+    //            });
 
 
-  //              var httpsServer = https.createServer(credentials, app);
-    //            httpsServer.listen(8443);
+                var httpsServer = https.createServer(credentials, app);
+                httpsServer.listen(8443);
             }
         ).catch(
             err => {
