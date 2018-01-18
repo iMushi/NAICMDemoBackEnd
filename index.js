bcb81f3be6fc160c7f7f1de21964f59df853bf8c
iMushi <imushi@me.com>
2018-01-17 21:53:57 -0600

    ultimo

--- index.js
+++ index.js
@@ -2,16 +2,14 @@
 
 var mongoose = require('mongoose')
 var app = require('./app');
+var fs = require('fs');
+var https = require('https');
+
 var port = process.env.PORT || 5000;
 var mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/ZOO"; //Configuración del servidor o localhost
 
 mongoURI = "mongodb://heroku_tqf52q58:q3uoqqehp1547bb721tt7e1872@ds133746.mlab.com:33746/heroku_tqf52q58";
-
-
-//kJSFiiPXdFWxceWK
-
 mongoose.Promise = global.Promise;
-
 
 const Mongod = require('mongod');
 
@@ -22,6 +20,10 @@ const Mongod = require('mongod');
 //});
 
 
+var privateKey  = fs.readFileSync('certificates/key.pem', 'utf8');
+var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 //server.open((err) => {
 //    console.log(err);
@@ -31,10 +33,15 @@ const Mongod = require('mongod');
             _ => {
                 console.log("conectado a MongoDB ====> " + mongoURI);
 
+                /*
                 app.listen(port, _ => {
+
                     console.log('Servidor Corriendo');
                 });
+                */
 
+                var httpsServer = https.createServer(credentials, app);
+                httpsServer.listen(8443);
             }
         ).catch(
             err => {
